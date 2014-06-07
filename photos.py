c6cb45e834e7990c98adb5fa89f48d281953ca16
Neil Stoker <neil.stoker@gmail.com>
2014-06-07 17:12:38 +0100

    text and image banners on print version

--- photos.py
+++ photos.py
@@ -18,59 +18,81 @@ import time
 import shutil
 import argparse
 import os
+from subprocess import call
+from time import sleep
 
-dummyRun=True #False
+dryRun=False
 facebook=False
 ignore_existing=False
 path_to_watch="new/"
 path_to_print="forPrinting/"
 path_to_upload="forUploading/"
 twitter=False
+bannerText=""
+logoSet={}
+imageMagicBanners=" "
+verbosity=0
 
 def main(argv):
     global path_to_watch
     global path_to_print
     global path_to_upload
+    global bannerText
+    global logoSet
+    global imageMagicBanners
+    global dryRun
+    global verbosity
+    
     process_initial_photos=True
     dry_run=False
     
     parser=argparse.ArgumentParser(
         description='Add banners and/or text to existing photographs and converting to a smaller image size for uploading to social media sites. ',
-        epilog='position can be one of n, ne, e, se, s, sw, w or nw')
+        epilog='position can be one of north, northeast, east, southeast, south, southwest, west or northwest')
     parser.add_argument("watch", help="Folder to watch for new photos")
     parser.add_argument("forPrinting", help="Folder to put modified pictures into")
     parser.add_argument("forUpload", help="Folder to put photos for upload")
-    parser.add_argument("-initial", help="Add banners to files already in folder (default off)", action="store_true")
-    parser.add_argument("-once",help="Run once only. Only valid if -initial is set", action="store_true")
-    parser.add_argument("--dry-run", help="Do a dry run without creating/adding logos or text. Displays commands to imagemagick", action="store_true")
-    parser.add_argument("-logo", help="Add image at position", action="append", nargs=2, metavar=('filename','position'))
-    parser.add_argument("-text", help="Add text at position", action="append", nargs=2, metavar=('text','position'))
+    parser.add_argument("-i","--initial", help="Add banners to files already in folder (default off)", action="store_true")
+    parser.add_argument("-1","--once",help="Run once only. Only valid if -initial is set", action="store_true")
+    parser.add_argument("-d","--dry-run", help="Do a dry run without creating/adding logos or text. Displays commands to imagemagick", action="store_true")
+    parser.add_argument("-l","--logo", help="Add image at position", action="append", nargs=2, metavar=('filename','position'))
+    parser.add_argument("-t","--text", help="Add text at position", action="append", nargs=2, metavar=('text','position'))
+    parser.add_argument("-m","--move", help="Move orginals to folder when completed",nargs=1, metavar=('foldername'))
+    parser.add_argument("-v","--verbose", help="Display more details about progress", action="store_true")
+    
     args=parser.parse_args()
     
     path_to_watch=args.watch
     path_to_print=args.forPrinting
     path_to_upload=args.forUpload
+    
+    if not(path_to_watch.endswith("\\") or path_to_watch.endswith("/")):
+        parser.print_help()
+        print
+        print "Error: make sure that the trailing directory marker is included"
+        exit(1)
+            
     process_initial_photos=args.initial
+    dryRun = args.dry_run
+    verbosity = args.verbose
     
     before = dict ([(f, None) for f in os.listdir (path_to_watch)])
-    
-    if process_initial_photos:
-        bannerise(before)
-    else:
-        print "Ignoring files already in the folder"
+
+    if args.logo:
+        logoSet.update(dict(args.logo))
         
     if args.text:
-        print args.text
-    else:
-        print "No text to be added"
-        
-    if args.logo:
-        print args.logo
-    else:
-        print "No logos to be added"
+        bannerText=args.text
+        textLogos(bannerText)
     
-    if not(args.text or args.logo):
-        print "Just doing resize - no logos or text will be added"
+    if logoSet:
+        print "Got some banners to do"
+        imageMagicBanners=imageConvert()
+        
+    print "logoSet '%s'" %(logoSet)
+
+    if process_initial_photos:
+        addBannersTo(before)
     
     print "\nScanning %s for new images, printable versions going in %s and uploadable versions saved in %s" %(path_to_watch,path_to_print,path_to_upload)
     
@@ -80,20 +102,82 @@ def main(argv):
         
         if added: 
           print "Added: ", ", ".join (added)
-          bannerise(added)
+          addBannersTo(added)
 
         if args.once:
-            break
             print "only running once"
+            break
             
         print "Waiting for more photo's to be added (snoozing for 10s)"
         before = after
         sleep(10)
 
-def bannerise(photos):
-    for p in photos:
-        print "Imagine the effect on %s" %(p)
-
+def textLogos(text):
+    global logoSet
+    print text
     
+    for i in range(len(text)):        
+        cmd = "convert -size 2000x300 xc:pink -pointsize 300 -gravity center -undercolor royalblue -stroke none -strokewidth 1 -fill gold -annotate +0+0 '" + text[i][0] +"' -trim +repage -shave 1x1 text%s.jpg" %(i)
+        if dryRun:
+            print "BuildLogos cmd is %s" %(cmd)
+        else:
+            if verbosity:
+                print "textLogos imageMagick command is %s" %(cmd)
+                
+            call( [cmd], shell=True)
 
-main(sys.argv)
+        logoSet["text%s.jpg"%(i)]=text[i][1]
+
+def imageConvert():
+    cmd=""
+    for l in logoSet:
+        cmd+=" -gravity " + logoSet[l] + " " + l + " -composite "
+    
+    # and the final border
+
+    return cmd + "-bordercolor none -border 100x100 "
+
+def Polariod():
+    # Create polaroid effect
+    return  " -gravity center -background none +polaroid "
+    
+def addBannersTo(photos):
+    for p in photos:
+        if p==".DS_Store":
+            continue
+        
+        fname=os.path.splitext(p)[0]
+        if os.path.splitext(p)[1].lower()==".orf":
+            fname+= ".jpg"
+        else:
+            fname+= os.path.splitext(p)[1]
+        print "fname '%s'" %(fname)
+        
+        srcF   = os.path.join(os.path.dirname(path_to_watch),p)
+        printF = os.path.join(os.path.dirname(path_to_print),fname)
+        print "srcF '%s' pathToWatch '%s' printF '%s'" %(srcF,path_to_watch,printF)
+        
+        cmd = "convert " + srcF + imageMagicBanners + printF
+        
+        if dryRun:
+            print "print command '%s'" %(cmd)
+        else:
+            if verbosity:
+                print "adding banners with '%s'" %(cmd)
+                
+            call([cmd],shell=True)
+            print "adding banners with %s" %(cmd)
+            
+        cmd = "convert " + srcF + imageMagicBanners + Polariod() + printF + "| convert " + printF + " -resize 640x480 " + printF
+        
+        if dryRun:
+            print "resize command: %s" %(cmd)
+        else:
+            if verbosity:
+                print "polaroid and resize with %s" %(cmd)
+                
+            #call([cmd],shell=True)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
